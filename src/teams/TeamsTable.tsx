d32ec223bf63dca771809434679e73ae89cf1b8b
Dragos Dumitras <dragos.dumitras@linnify.com>
2023-08-11 20:09:35 +0300

    use teams from loadTeamsRequest

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { loadTeamsRequest } from "./middleware";
 
 /* eslint-disable no-template-curly-in-string */
 type Team = {
@@ -111,27 +112,28 @@ type WrapperProps = {};
 
 type State = {
   loading: boolean;
+  teams: Team[];
 };
 
 export class TeamsTableWrapper extends React.Component<WrapperProps, State> {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
+      loading: true,
+      teams: []
     };
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      // this.state.loading = false; // is read-only
-      this.setState({ loading: false });
-    }, 5000);
+  async componentDidMount() {
+    const teams = await loadTeamsRequest();
+    this.setState({
+      loading: false,
+      teams
+    });
   }
 
   render() {
     console.info("render", this.state.loading);
-    let teams: Team[] = [];
-
-    return <TeamsTable loading={this.state.loading} teams={teams} />;
+    return <TeamsTable loading={this.state.loading} teams={this.state.teams} />;
   }
 }
