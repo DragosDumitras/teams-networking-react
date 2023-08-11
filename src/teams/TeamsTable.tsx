fa62bc19c7e97e599abf08ef24fbaa25de6be429
Dragos Dumitras <dragos.dumitras@linnify.com>
2023-08-11 19:47:22 +0300

    convert TeamsTableWrapper to React.Component class

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 /* eslint-disable no-template-curly-in-string */
 type Team = {
   id: string;
@@ -105,8 +107,31 @@ export function TeamsTable(props: Props) {
   );
 }
 
-export function TeamsTableWrapper() {
-  let teams: Team[] = [];
+type WrapperProps = {};
 
-  return <TeamsTable loading={false} teams={teams} />;
+type State = {
+  loading: boolean;
+};
+
+export class TeamsTableWrapper extends React.Component<WrapperProps, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      // this.state.loading = false; // is read-only
+      this.setState({ loading: false });
+    }, 5000);
+  }
+
+  render() {
+    console.info("render", this.state.loading);
+    let teams: Team[] = [];
+
+    return <TeamsTable loading={this.state.loading} teams={teams} />;
+  }
 }
