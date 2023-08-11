ceccf13da258f96092c02ae36dc611ddc9efe853
Dragos Dumitras <dragos.dumitras@linnify.com>
2023-08-11 20:47:48 +0300

    add deleteTeam action (callback)

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -11,7 +11,13 @@ type Team = {
   createdBy?: string;
 };
 
-function TeamRow(props: { team: Team }) {
+type TeamRowProps = {
+  team: Team;
+  // deteleTeam1: (id: string) => void;
+  deleteTeam(id: string): void;
+};
+
+function TeamRow(props: TeamRowProps) {
   const team: Team = props.team;
   // const id = team.id;
   // const url = team.url;
@@ -44,9 +50,7 @@ function TeamRow(props: { team: Team }) {
           type="button"
           className="action-btn delete-btn"
           onClick={async () => {
-            console.warn("delete", id, team);
-            await deleteTeamRequest(id);
-            window.location.reload();
+            props.deleteTeam(id);
           }}
         >
           ♻
@@ -89,7 +93,16 @@ export function TeamsTable(props: Props) {
         </thead>
         <tbody>
           {props.teams.map(team => (
-            <TeamRow key={team.id} team={team} />
+            <TeamRow
+              key={team.id}
+              team={team}
+              deleteTeam={id => {
+                console.warn("pls remove %o", id);
+                // console.warn("delete", id, team);
+                // await deleteTeamRequest(id);
+                // window.location.reload();
+              }}
+            />
           ))}
         </tbody>
         <tfoot>
